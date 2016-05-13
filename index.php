845c28e64f0d8911af42ee8cf2741578ee15adf1
Guenther Schmitz <gpunktscripts@gpunktschmitz.com>
2016-05-13 23:20:52 +0200

    quick fix for multiple devices

--- index.php
+++ index.php
@@ -1,7 +1,53 @@
+<?php
+
+require('login.inc.php');
+
+$ch = curl_init('http://fritz.box/login_sid.lua');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$login = curl_exec($ch);
+$session_status_simplexml = simplexml_load_string($login);
+if($session_status_simplexml->SID != '0000000000000000') {
+	$SID = $session_status_simplexml->SID;
+} else {
+	$challenge = $session_status_simplexml->Challenge;
+	$response = $challenge . '-' . md5(mb_convert_encoding($challenge.'-'.$fritzbox_password, "UCS-2LE", "UTF-8"));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "username=".$fritzbox_username."&response={$response}&page=/login_sid.lua");
+	$sendlogin = curl_exec($ch);
+	$session_status_simplexml = simplexml_load_string($sendlogin);
+	if($session_status_simplexml->SID != '0000000000000000') {
+		$SID = $session_status_simplexml->SID;
+	} else {
+		echo "ERROR: Login failed";
+		return;
+	}
+}
+curl_close($ch);
+
+$devicesXML = simplexml_load_string(file_get_contents('http://fritz.box/webservices/homeautoswitch.lua?switchcmd=getdevicelistinfos&sid='.$SID));
+
+$outputDevicesArray = Array();
+$counter = 0;
+foreach($devicesXML->device as $device) {
+	$arr = Array();
+	$arr['name'] = $device->name->__toString();
+	$arr['id'] = ++$counter;
+	$arr['state'] = $device->switch->state->__toString();
+	$arr['identifier'] = str_replace(" ", '', $device[identifier]->__toString());
+	if($arr['state']) {
+		$arr['switch_state'] = ' checked="checked"';
+	} else {
+		$arr['switch_state'] = '';
+	}
+	$outputDevicesArray[] = $arr;
+}
+
+$logout = file_get_contents('http://fritz.box/login.lua?page=/home/home.lua&logout=1&sid='.$SID);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<script src="./js/jquery-2.2.2.min.js"></script>
+<script src="/js/jquery-2.2.2.min.js"></script>
 <style>
 body {
   font-family: sans;
@@ -16,12 +62,39 @@ table {
   box-sizing: border-box;
 }
 
-#switch {
+<?php foreach($outputDevicesArray as $deviceArray) { ?>
+#switch<?php echo $deviceArray['id'] ?> {
   visibility: hidden;
   clip: rect(0 0 0 0);
   position: absolute;
   left: 9999px;
 }
+
+#switch<?php echo $deviceArray['id'] ?>:checked ~ .switch {
+  background: #b7bfc2;
+  /* Old browsers */
+  background: -moz-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
+  /* FF3.6+ */
+  background: -webkit-gradient(linear, left top, right top, color-stop(0%, #b7bfc2), color-stop(26%, #e1e9ec), color-stop(32%, #fff9f4), color-stop(38%, #d4dcdf), color-stop(66%, #ccd4d7), color-stop(71%, #d8e0e3), color-stop(100%, #ced8da));
+  /* Chrome,Safari4+ */
+  background: -webkit-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
+  /* Chrome10+,Safari5.1+ */
+  background: -o-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
+  /* Opera 11.10+ */
+  background: -ms-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
+  /* IE10+ */
+  background: linear-gradient(to right, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
+  /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#b7bfc2', endColorstr='#ced8da',GradientType=1 );
+  /* IE6-9 */
+}
+
+#switch<?php echo $deviceArray['id'] ?>:checked ~ .switch:after {
+  background: #b1ffff;
+  box-shadow: inset 0 -1px 8px rgba(0, 0, 0, 0.7), inset 0 -2px 2px rgba(0, 0, 0, 0.2), 0 1px 0 white, 0 -1px 0 rgba(0, 0, 0, 0.5), -110px 32px 15px 13px rgba(0, 0, 0, 0.25);
+}
+
+<?php } ?>
 
 .switch {
   display: block;
@@ -82,30 +155,6 @@ table {
   box-shadow: inset 0 -1px 8px rgba(0, 0, 0, 0.7), inset 0 -2px 2px rgba(0, 0, 0, 0.2), 0 1px 0 white, 0 -1px 0 rgba(0, 0, 0, 0.5), -47px 32px 15px 13px rgba(0, 0, 0, 0.25);
 }
 
-#switch:checked ~ .switch {
-  background: #b7bfc2;
-  /* Old browsers */
-  background: -moz-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
-  /* FF3.6+ */
-  background: -webkit-gradient(linear, left top, right top, color-stop(0%, #b7bfc2), color-stop(26%, #e1e9ec), color-stop(32%, #fff9f4), color-stop(38%, #d4dcdf), color-stop(66%, #ccd4d7), color-stop(71%, #d8e0e3), color-stop(100%, #ced8da));
-  /* Chrome,Safari4+ */
-  background: -webkit-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
-  /* Chrome10+,Safari5.1+ */
-  background: -o-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
-  /* Opera 11.10+ */
-  background: -ms-linear-gradient(left, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
-  /* IE10+ */
-  background: linear-gradient(to right, #b7bfc2 0%, #e1e9ec 26%, #fff9f4 32%, #d4dcdf 38%, #ccd4d7 66%, #d8e0e3 71%, #ced8da 100%);
-  /* W3C */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#b7bfc2', endColorstr='#ced8da',GradientType=1 );
-  /* IE6-9 */
-}
-
-#switch:checked ~ .switch:after {
-  background: #b1ffff;
-  box-shadow: inset 0 -1px 8px rgba(0, 0, 0, 0.7), inset 0 -2px 2px rgba(0, 0, 0, 0.2), 0 1px 0 white, 0 -1px 0 rgba(0, 0, 0, 0.5), -110px 32px 15px 13px rgba(0, 0, 0, 0.25);
-}
-
 #devicetitle {
   color: #fff;
   font-size: 3em;
@@ -131,60 +180,19 @@ table {
 <body>
 <div id="content">
 <table>
-<?php
-
-require('login.inc.php');
-
-$ch = curl_init('http://fritz.box/login_sid.lua');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$login = curl_exec($ch);
-$session_status_simplexml = simplexml_load_string($login);
-if($session_status_simplexml->SID != '0000000000000000') {
-	$SID = $session_status_simplexml->SID;
-} else {
-	$challenge = $session_status_simplexml->Challenge;
-	$response = $challenge . '-' . md5(mb_convert_encoding($challenge.'-'.$fritzbox_password, "UCS-2LE", "UTF-8"));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "username=".$fritzbox_username."&response={$response}&page=/login_sid.lua");
-	$sendlogin = curl_exec($ch);
-	$session_status_simplexml = simplexml_load_string($sendlogin);
-	if($session_status_simplexml->SID != '0000000000000000') {
-		$SID = $session_status_simplexml->SID;
-	} else {
-		echo "ERROR: Login failed";
-		return;
-	}
-}
-curl_close($ch);
-
-$devicesXML = simplexml_load_string(file_get_contents('http://fritz.box/webservices/homeautoswitch.lua?switchcmd=getdevicelistinfos&sid='.$SID));
-
-$outputDevicesArray = Array();
-foreach($devicesXML->device as $device) {
-	$arr = Array();
-	$arr['name'] = $device->name->__toString();
-	$arr['state'] = $device->switch->state->__toString();
-	$arr['identifier'] = str_replace(" ", '', $device[identifier]->__toString());
-	if($arr['state']) {
-		$arr['switch_state'] = ' checked="checked"';
-	} else {
-		$arr['switch_state'] = '';
-	}
-	$outputDevicesArray[] = $arr;
-}
-
-$logout = file_get_contents('http://fritz.box/login.lua?page=/home/home.lua&logout=1&sid='.$SID);
-
-foreach($outputDevicesArray as $deviceArray) { ?>
-<tr><td><span id="devicetitle"><?php echo $deviceArray['name']?></span></td><td><input type="checkbox" id="switch" name="switch" ain="<?php echo $deviceArray['identifier'] ?>" <?php echo $deviceArray['switch_state']?>><label for="switch" class="switch"></label></td></tr>
+<?php foreach($outputDevicesArray as $deviceArray) { ?>
+<tr><td><span id="devicetitle"><?php echo $deviceArray['name']?></span></td><td><input type="checkbox" id="switch<?php echo $deviceArray['id']?>" name="switch<?php echo $deviceArray['id']?>" ain="<?php echo $deviceArray['identifier'] ?>" <?php echo $deviceArray['switch_state']?>><label for="switch<?php echo $deviceArray['id']?>" class="switch"></label></td></tr>
 <?php } ?>
 </table>
 </div>
 <script>
-$("#switch").click( function(){
+<?php foreach($outputDevicesArray as $deviceArray) { ?>
+$("#switch<?php echo $deviceArray['id']?>").click( function(){
     var ain = $(this).attr('ain');
 	if($(this).is(':checked')){$.get("switchOn.php?ain="+ain)};
 	if(!$(this).is(':checked')){$.get("switchOff.php?ain="+ain)};
 });
+<?php } ?>
 </script>
 </body>
 </html>
